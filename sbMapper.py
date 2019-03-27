247fd4a57f97c8657f54daa5fa817c2b4ed36742
BURNS-KIRKNESS Rowan <bk@rowan.engineer>
2019-03-27 18:21:17 +0900

    beeg changes

--- sbMapper.py
+++ sbMapper.py
@@ -1,5 +1,6 @@
 import xlrd
 import sys
+import argparse
 
 def readExcelFile(file):
     book = xlrd.open_workbook(file)
@@ -33,7 +34,7 @@ def readExcelFile(file):
             teirList.update({sheet.cell(r1,5).value:sheet.cell(r1,6).value})
 
     writelist(main,side)
-    label = list(set(main)|set(side))
+    #label = list(set(main)|set(side))
     return(scoreListDict)
 
 def writelist(main,side):
@@ -43,21 +44,21 @@ def writelist(main,side):
         count,card = card.rstrip().split(' ',1)
         maindeck[card] = int(count)
     for card in main:
-        card = card.rstrip('1234567890')
+        card = card.rstrip('!')
         if card in maindeck:
             maindeck[card] += 1
         else:
             maindeck[card] = 1
     for card in side:
-        card = card.rstrip('1234567890')
+        card = card.rstrip('!')
         if card in sideboard:
             sideboard[card] += 1
         else:
             sideboard[card] = 1
-    for card,count in sorted(maindeck.items(),key=lambda x: x[1],reverse=True):
+    for card,count in sorted(maindeck.items(),key=lambda x: str(x[1])+x[0],reverse=True):
         outfile.write(str(count)+' '+card+'\n')
     outfile.write('Sideboard\n')
-    for card,count in sorted(sideboard.items(),key=lambda x: x[1],reverse=True):
+    for card,count in sorted(sideboard.items(),key=lambda x: str(x[1])+x[0],reverse=True):
         outfile.write(str(count)+' '+card+'\n')
     outfile.write('\n')
 
@@ -68,8 +69,6 @@ def evaluate(name,scoreList):
             scoreList.update({card:scoreList[card]+0.2})
     cards = sorted(scoreList, key=scoreList.get)
     out = 0
-    valOut = 0
-    valIn = 0
     outfile.write('~'+name+'\n')
     inDict = {}
     outDict = {}
@@ -83,7 +82,6 @@ def evaluate(name,scoreList):
             if out>=15:
                 lines2write.append("+"+str(copies)+' '+str(card))
                 currScore=round(scoreList[card])
-                valIn += copies*currScore
                 inDict.update({card:round(scoreList[card])})
             else:
                 out+=copies
@@ -98,31 +96,46 @@ def evaluate(name,scoreList):
                 out+=copies
                 if out >= 15:
                     currScore=(scoreList[card]-0.2)
-                    valOut+=copies*currScore
             else:
                 inDict.update({card:round(scoreList[card])})
-    val = (valIn-valOut)
+    max_effic = len(main)*MAX_SCORE
+    totval = 0
+    for card in main:
+        totval += min(MAX_SCORE,round(scoreList[card]))
+    preScore = str(int((100*totval)/max_effic))
+
+    if len(lines2write) == 0:
+        outfile.write('No changes, efficacy '+preScore+'%\n\n')
+        return(totval)
+
+    valOut = 0
+    valIn = 0
+    for count,card in [c[1:].split(' ',1) for c in lines2write if '+' in c]:
+        valIn += int(count)*min(MAX_SCORE,round(scoreList[card]))
+    for count,card in [c[1:].split(' ',1) for c in lines2write if '-' in c]:
+        valOut += int(count)*min(MAX_SCORE,round(scoreList[card]))
+    val = valIn - valOut
+
     firstChar = lines2write[0][0]
     lines2write.sort(reverse=True)
     l2wNoDup = [lines2write[0]]
     for i in range(1,len(lines2write)):
-        if lines2write[i-1][3:].rstrip('1234567890') == lines2write[i][3:].rstrip('1234567890'):
+        if lines2write[i-1][3:].rstrip('!') == lines2write[i][3:].rstrip('!'):
             extras = lines2write[i-1]
             count,_ = extras[1:].split(' ',1)
             l = lines2write[i]
-            print(l2wNoDup)
             l2wNoDup[-1] = l[0]+str(int(l[1])+int(count))+l[2:]
-            print(l2wNoDup)
-            print()
         else:
             l2wNoDup.append(lines2write[i])
     lines2write = l2wNoDup
+
     for line in lines2write:
-        if line[0] != firstChar or line == lines2write[-1]:
-            outfile.write(line.rstrip('1234567890')+'\n')
+        if line[0] != firstChar:
+            outfile.write('\n')
             firstChar = line[0]
-        else:
-            outfile.write(line+', ')
+        outfile.write(line.rstrip('!')+' ')
+        if line == lines2write[-1]:
+            outfile.write('\n')
     maxOut = round(sorted(outDict.values(),reverse=True)[0])
     minIn = round(sorted(inDict.values())[0])
     #print(name,outDict,'\n',inDict)
@@ -136,31 +149,51 @@ def evaluate(name,scoreList):
             if mcopies>0 and round(scoreList[card]) == minIn:
                 outfile.write(" M"+str(mcopies)+"_"+card)
         outfile.write('\n')
-    if valIn == 0 and valOut == 0:
-        outfile.write("No changes")
-    else:
-        outfile.write('#Score '+str(val))
+    posScore = str(int((100*(totval+val))/max_effic))
+    outfile.write('#Efficacy '+posScore+'% improved from '+preScore+'% preboard\n')
     outfile.write('\n\n')
-    return(val)
+    return(int(posScore))
 
-main = []
-side =[] 
+def inc(cc):
+    for p in reversed(range(len(cc)-1)):
+        if cc[p] < 4:
+            cc[p] += 1
+            return p,cc
+    return None
+
+def dec(cc,d):
+    for p in range(d+1,len(cc)):
+        if cc[p] > 0:
+            cc[p] -= 1
+            return p,cc
+    return None
+
+def eval_xlsx(fname):
+    scoreListDict = readExcelFile(fname)
+    deckScore = 0
+    for name in sorted(scoreListDict.keys()):
+        teirModifier = teirList[name]
+        indivScore = evaluate(name,scoreListDict[name])*teirModifier
+        deckScore+=indivScore
+    outval = str(deckScore/sum(teirList.values()))
+    outfile.write("Average postboard efficacy:"+outval+"%\n")
+    print("Average postboard efficacy:"+outval+"%\n")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path',nargs='?')
+parser.add_argument('-o',nargs='?', default="pre_map.txt")
+parser.add_argument('-m',nargs='?', default=4)
+args = parser.parse_args()
+
 teirList = {}
 outOf = 0
 #scoreListDict = readTextfile("./list.txt")
+fname = vars(args)['path']
 basefile = open("baselist.txt",'r')
-if len(sys.argv)>2:
-    outfile = open(sys.argv[2],"w")
-else:
-    outfile = open("pre_map.txt","w")
-if len(sys.argv)>1:
-    scoreListDict = readExcelFile(sys.argv[1])
-else:
-    scoreListDict = readExcelFile("./currStorm.xlsx")
-deckScore = 0
-for name in sorted(scoreListDict.keys()):
-    teirModifier = teirList[name]
-    indivScore = evaluate(name,scoreListDict[name])*teirModifier
-    deckScore+=indivScore
-outval = deckScore/sum(teirList.values())
-outfile.write("Total score:{: .1f}".format(outval))
+outfile = open(vars(args)['o'],"w")
+MAX_SCORE = int(vars(args)['m'])
+main = []
+side = []
+
+if __name__ == "__main__":
+    eval_xlsx(fname)
