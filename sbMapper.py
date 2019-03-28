20823c3b60109080eabcc76bd7dff0cfa5cc96c9
BURNS-KIRKNESS Rowan <bk@rowan.engineer>
2019-03-28 17:56:12 +0900

    Calling it good enough for now

--- sbMapper.py
+++ sbMapper.py
@@ -6,6 +6,9 @@ def readExcelFile(file):
     book = xlrd.open_workbook(file)
     sheet = book.sheet_by_index(0)
     scoreListDict = {}
+    teirList = {}
+    main = []
+    side = []
     for row in range(sheet.nrows-1):
         if sheet.cell_type(1+row,1)!=0:
             for i in range(int(sheet.cell(1+row,0).value)):
@@ -33,114 +36,110 @@ def readExcelFile(file):
             scoreListDict.update({sheet.cell(r1,5).value:rating})
             teirList.update({sheet.cell(r1,5).value:sheet.cell(r1,6).value})
 
-    writelist(main,side)
     #label = list(set(main)|set(side))
-    return(scoreListDict)
+    return(scoreListDict,main,side,teirList)
 
-def writelist(main,side):
-    maindeck = {}
-    sideboard = {}
-    for card in basefile.readlines():
-        count,card = card.rstrip().split(' ',1)
-        maindeck[card] = int(count)
-    for card in main:
-        card = card.rstrip('!')
-        if card in maindeck:
-            maindeck[card] += 1
-        else:
-            maindeck[card] = 1
-    for card in side:
-        card = card.rstrip('!')
-        if card in sideboard:
-            sideboard[card] += 1
-        else:
-            sideboard[card] = 1
-    for card,count in sorted(maindeck.items(),key=lambda x: str(x[1])+x[0],reverse=True):
-        outfile.write(str(count)+' '+card+'\n')
-    outfile.write('Sideboard\n')
-    for card,count in sorted(sideboard.items(),key=lambda x: str(x[1])+x[0],reverse=True):
-        outfile.write(str(count)+' '+card+'\n')
-    outfile.write('\n')
+def writelist(main,side,basefn,outfile):
+    with open(basefn,'r') as basefile:
+        maindeck = {}
+        sideboard = {}
+        for card in basefile.readlines():
+            count,card = card.rstrip().split(' ',1)
+            maindeck[card] = int(count)
+        for card in main:
+            card = card.rstrip('!')
+            if card in maindeck:
+                maindeck[card] += 1
+            else:
+                maindeck[card] = 1
+        for card in side:
+            card = card.rstrip('!')
+            if card in sideboard:
+                sideboard[card] += 1
+            else:
+                sideboard[card] = 1
+        for card,count in sorted(maindeck.items(),key=lambda x: str(x[1])+x[0],reverse=True):
+            outfile.write(str(count)+' '+card+'\n')
+        outfile.write('Sideboard\n')
+        for card,count in sorted(sideboard.items(),key=lambda x: str(x[1])+x[0],reverse=True):
+            outfile.write(str(count)+' '+card+'\n')
+        outfile.write('\n')
 
-
-def evaluate(name,scoreList):
+def evaluate(name,scoreList,main,side,outfile,max_score):
     for card in scoreList:
         if card in main:
             scoreList.update({card:scoreList[card]+0.2})
     cards = sorted(scoreList, key=scoreList.get)
     out = 0
     outfile.write('~'+name+'\n')
-    inDict = {}
-    outDict = {}
     lines2write = []
+    bringIn = {}
+    takeOut = {}
+    extras = []
+
+    maxOut = 0
+    minIn = max_score
+    rem = 15
     for card in cards:
-        if card in side:
-            copies = side.count(card)
-            if out<15 and out+copies>15:
-                copies = out+copies-15
-                out = 15
-            if out>=15:
-                lines2write.append("+"+str(copies)+' '+str(card))
-                currScore=round(scoreList[card])
-                inDict.update({card:round(scoreList[card])})
+        cardScore = round(scoreList[card])
+        if '!' in card:
+            card = card.rstrip('!')
+        mc = main.count(card)
+        sc = side.count(card)
+        if mc == 0 and sc == 0:
+            continue
+        if mc + sc <= rem:
+            maxOut = cardScore
+            rem -= mc + sc
+            if mc > 0:
+                if card in takeOut:
+                    takeOut[card] += mc
+                else:
+                    takeOut[card] = mc
+        elif rem == 0:
+            minIn = min(minIn,cardScore)
+            if sc > 0:
+                bringIn[card] = sc
+        else:
+            if sc <= rem:
+                rem -= sc
             else:
-                out+=copies
-                outDict.update({card:round(scoreList[card])})
-        if card in main:
-            if out<15:
-                outDict.update({card:round(scoreList[card])})
-                copies = main.count(card)
-                if copies+out>15:
-                    copies=15-out
-                lines2write.append("-"+str(copies)+' '+str(card))
-                out+=copies
-                if out >= 15:
-                    currScore=(scoreList[card]-0.2)
-            else:
-                inDict.update({card:round(scoreList[card])})
-    max_effic = len(main)*MAX_SCORE
+                bringIn[card] = sc - rem
+                minIn = min(minIn,cardScore)
+                if rem < sc:
+                    maxOut = cardScore
+                rem = 0
+            if rem > 0:
+                if mc > 0:
+                    if card in takeOut:
+                        takeOut[card] += rem
+                    else:
+                        takeOut[card] = rem
+                rem = 0
+    max_effic = len(main)*max_score
     totval = 0
     for card in main:
-        totval += min(MAX_SCORE,round(scoreList[card]))
+        totval += min(max_score,round(scoreList[card]))
     preScore = str(int((100*totval)/max_effic))
 
-    if len(lines2write) == 0:
-        outfile.write('No changes, efficacy '+preScore+'%\n\n')
+    if len(bringIn) == 0 and len(takeOut) == 0:
+        outfile.write('#No changes, efficacy '+preScore+'%\n\n')
         return(totval)
 
     valOut = 0
     valIn = 0
-    for count,card in [c[1:].split(' ',1) for c in lines2write if '+' in c]:
-        valIn += int(count)*min(MAX_SCORE,round(scoreList[card]))
-    for count,card in [c[1:].split(' ',1) for c in lines2write if '-' in c]:
-        valOut += int(count)*min(MAX_SCORE,round(scoreList[card]))
+    for card,count in sorted(takeOut.items(),key=lambda x: str(x[1])+x[0],reverse = True):
+        outfile.write('-'+str(count)+' '+card+' ')
+        valOut += int(count)*min(max_score,round(scoreList[card]))
+    outfile.write('\n')
+    for card,count in sorted(bringIn.items(),key=lambda x: str(x[1])+x[0],reverse = True):
+        outfile.write('+'+str(count)+' '+card+' ')
+        valIn += int(count)*min(max_score,round(scoreList[card]))
+
+    outfile.write('\n')
     val = valIn - valOut
-
-    firstChar = lines2write[0][0]
-    lines2write.sort(reverse=True)
-    l2wNoDup = [lines2write[0]]
-    for i in range(1,len(lines2write)):
-        if lines2write[i-1][3:].rstrip('!') == lines2write[i][3:].rstrip('!'):
-            extras = lines2write[i-1]
-            count,_ = extras[1:].split(' ',1)
-            l = lines2write[i]
-            l2wNoDup[-1] = l[0]+str(int(l[1])+int(count))+l[2:]
-        else:
-            l2wNoDup.append(lines2write[i])
-    lines2write = l2wNoDup
-
-    for line in lines2write:
-        if line[0] != firstChar:
-            outfile.write('\n')
-            firstChar = line[0]
-        outfile.write(line.rstrip('!')+' ')
-        if line == lines2write[-1]:
-            outfile.write('\n')
-    maxOut = round(sorted(outDict.values(),reverse=True)[0])
-    minIn = round(sorted(inDict.values())[0])
-    #print(name,outDict,'\n',inDict)
     if maxOut == minIn:
-        outfile.write('#Duplicate '+str(minIn)+'\'s: ')
+        outfile.write('#Chose between multiple '+str(minIn)+'\'s: ')
         for card in cards:
             mcopies = main.count(card)
             scopies = side.count(card)
@@ -168,32 +167,34 @@ def dec(cc,d):
             return p,cc
     return None
 
-def eval_xlsx(fname):
-    scoreListDict = readExcelFile(fname)
+def eval_list(scoreListDict,main,side,teirList,outfile,max_score):
     deckScore = 0
+    scores = {}
     for name in sorted(scoreListDict.keys()):
         teirModifier = teirList[name]
-        indivScore = evaluate(name,scoreListDict[name])*teirModifier
+        indivScore = evaluate(name,scoreListDict[name].copy(),main,side,outfile,max_score)
+        scores[name] = indivScore
+        indivScore *= teirModifier
         deckScore+=indivScore
     outval = str(deckScore/sum(teirList.values()))
-    outfile.write("Average postboard efficacy:"+outval+"%\n")
+    outfile.write("#Average postboard efficacy:"+outval+"%\n")
+    for d,s in sorted(scores.items(),key=lambda x: x[1]):
+        print(d,s)
     print("Average postboard efficacy:"+outval+"%\n")
 
-parser = argparse.ArgumentParser()
-parser.add_argument('path',nargs='?')
-parser.add_argument('-o',nargs='?', default="pre_map.txt")
-parser.add_argument('-m',nargs='?', default=4)
-args = parser.parse_args()
-
-teirList = {}
-outOf = 0
-#scoreListDict = readTextfile("./list.txt")
-fname = vars(args)['path']
-basefile = open("baselist.txt",'r')
-outfile = open(vars(args)['o'],"w")
-MAX_SCORE = int(vars(args)['m'])
-main = []
-side = []
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path',nargs='?')
+    parser.add_argument('-o',nargs='?', default="pre_map.txt")
+    parser.add_argument('-m',nargs='?', default=6)
+    args = parser.parse_args()
+    fname = vars(args)['path']
+    basefile = "baselist.txt"
+    outfile = open(vars(args)['o'],"w")
+    max_score = int(vars(args)['m'])
+    scoreListDict,main,side,teirList = readExcelFile(fname)
+    writelist(main,side,basefile,outfile)
+    eval_list(scoreListDict,main,side,teirList,outfile,max_score)
 
 if __name__ == "__main__":
-    eval_xlsx(fname)
+    argparser()
